3f8c0cc472b39d339f2932a4729ef490582a6555
kirill.fedorov <kirillvladimirovich2512@yandex.ru>
2025-08-11 00:36:01 +0500

    настроил структуру stat-server

---
+++ stat/stat-server/src/main/java/ru/practicum/ewm/server/EwmStatServer.java
@@ -0,0 +1,11 @@
+package ru.practicum.ewm.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwmStatServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EwmStatServer.class, args);
+    }
+}
