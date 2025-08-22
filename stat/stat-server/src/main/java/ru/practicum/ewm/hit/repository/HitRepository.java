3806ad34dd4186769af913fcac09cc7d847a9650
kirill.fedorov <kirillvladimirovich2512@yandex.ru>
2025-08-22 23:52:08 +0500

    Фикс checkstyle

--- stat/stat-server/src/main/java/ru/practicum/ewm/hit/repository/HitRepository.java
+++ stat/stat-server/src/main/java/ru/practicum/ewm/hit/repository/HitRepository.java
@@ -11,7 +11,6 @@ import java.util.List;
 public interface HitRepository extends JpaRepository<Hit, Long> {
 
     @Query("""
-            
             SELECT h.uri as uri, h.app as app, count(h.ip) AS hits
                 FROM Hit h
                 WHERE timestamp BETWEEN :start AND :end
@@ -23,7 +22,6 @@ public interface HitRepository extends JpaRepository<Hit, Long> {
                                       @Param("uris") List<String> uris);
 
     @Query("""
-            
             SELECT h.uri as uri, h.app as app, count(DISTINCT h.ip) AS hits
                 FROM Hit h
                 WHERE timestamp BETWEEN :start AND :end
